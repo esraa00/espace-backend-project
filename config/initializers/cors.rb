f718b68995617496fe81e28711d9b5859217ef51
Esraa Mohamed Abodeif <esraa.abodeif1@gmail.com>
2023-12-13 02:11:53 +0200

    chore: allow put and patch http methods

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:5173', '127.0.0.1:5173'
-    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true, expose: %w(Authorization)
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :options], credentials: true, expose: %w(Authorization content-length)
   end
 end
